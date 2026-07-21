62bf941381e9fa802a210df692284359c748433f
Varun Jammalamadaka <cidreee7@gmail.com>
2026-07-21 17:59:19 -0400

    Improve portfolio typography with Inter and Manrope

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '@fontsource/inter'
+import '@fontsource/manrope'
 import './index.css'
 import App from './App.jsx'
 
